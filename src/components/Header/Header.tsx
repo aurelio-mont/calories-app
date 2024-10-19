4b063da686dacf4108df42f17ec9564252a06412
Aurelio Mont <aurelio.mont25@gmail.com>
2024-10-18 18:57:56 -0600

    Add input and button search

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -55,7 +55,9 @@ const styles = StyleSheet.create({
     height: 40,
     borderRadius: 24,
   },
-  arrowContainer: {},
+  arrowContainer: {
+    marginLeft: -12,
+  },
 });
 
 export default Header;
--- src/views/AddFood/AddFood.tsx
+++ src/views/AddFood/AddFood.tsx
@@ -1,13 +1,72 @@
-import { Text, View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 import Header from "../../components/Header";
+import { Button, Icon, Input } from "@rneui/themed";
 
 const AddFood = () => {
   return (
-    <View>
+    <View style={styles.container}>
       <Header />
-      <Text>AddFood</Text>
+      <View style={styles.addFoodContainer}>
+        <View style={styles.legendContainer}>
+          <Text style={styles.addFoodLegend}>Agregar alimento</Text>
+        </View>
+        <View style={styles.addFoodBtnContainer}>
+          <Button
+            icon={<Icon name="add-circle-outline" color={"white"} />}
+            radius={"lg"}
+            color={"success"}
+          />
+        </View>
+      </View>
+
+      <View style={styles.searchContainer}>
+        <View style={styles.inputContainer}>
+          <Input placeholder="Manzana, pan, cafe..." />
+        </View>
+        <Button
+          title={"Buscar"}
+          color={"#ade8af"}
+          radius={"lg"}
+          titleStyle={styles.searchBtnTitle}
+        />
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 12,
+    backgroundColor: "white",
+  },
+  addFoodContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    marginVertical: 24,
+  },
+  legendContainer: {
+    flex: 1,
+  },
+  addFoodBtnContainer: {
+    flex: 1,
+    alignItems: "flex-end",
+    justifyContent: "center",
+  },
+  addFoodLegend: {
+    fontSize: 18,
+  },
+  searchContainer: {
+    flexDirection: "row",
+  },
+  inputContainer: {
+    flex: 1,
+    marginLeft: -12,
+  },
+  searchBtnTitle: {
+    fontSize: 14,
+    color: "gray",
+  },
+});
 
 export default AddFood;
